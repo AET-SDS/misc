07b2e73f58afdb9e2293e035f053d2f90f57f468
Filip Bugaj <filip.bugaj@cognifide.com>
2019-11-14 19:13:24 +0100

    unzip_artifacts script added

---
+++ unzip_artifacts.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -o errexit
+
+AET_REPO_PATH=${1:-'../aet'}
+AET_DOCKER_PATH=${2:-'../aet-docker'}
+
+unzip_artifact() {
+    # $1 - zip to unpack
+    # rm -rf "${AET_DOCKER_PATH:?}/$1"
+    unzip -oq "$AET_REPO_PATH/zip/target/packages-$AET_VERSION/$1" -d "$AET_DOCKER_PATH/$1"
+}
+
+if [ ! -d "$AET_REPO_PATH" ] || [ ! -d "$AET_DOCKER_PATH" ]; then
+    echo -e "$AET_REPO_PATH or $AET_DOCKER_PATH doesn't exists.\nexiting..."
+    exit 1
+fi
+
+AET_VERSION=$(grep -E ".*<version>.*" "$AET_REPO_PATH/pom.xml" | head -n 1 | tr -dc '0-9|.|[A-Z]|-')
+
+for app in 'configs' 'bundles' 'features' 'report'; do
+    echo "Checking if $AET_DOCKER_PATH/$app exist..."
+    [ -d "$AET_DOCKER_PATH/$app" ] || mkdir -p "$AET_DOCKER_PATH/$app"
+    echo "Unzipping $app"
+    unzip_artifact $app
+done
+
+sudo chown aet:aet -R "$AET_DOCKER_PATH"
+
+echo "Finished copying artifacts"
